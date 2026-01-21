12c8beddc96e471a5eb8ee941482dee66d5ea306
Hsu Myat Noe <hsumyatnoe134@gmail.com>
2026-01-21 20:37:04 +0800

    level-0:renamed, greet, and exit

---
+++ src/main/java/Tomato.java
@@ -0,0 +1,10 @@
+public class Tomato {
+    public static void main(String[] args) {
+        String spacer = "____________________________________________________________";
+        System.out.println(spacer);
+        System.out.println("Hello! I'm Tomato \nWhat can I do for you?");
+        System.out.println(spacer);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(spacer);
+    }
+}
