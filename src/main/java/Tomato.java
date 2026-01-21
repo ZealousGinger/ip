615b20a69f01d29ee4eb4c4544ce97e268a9bd87
Hsu Myat Noe <hsumyatnoe134@gmail.com>
2026-01-21 21:23:08 +0800

    added echo

--- src/main/java/Tomato.java
+++ src/main/java/Tomato.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Tomato {
     public static void main(String[] args) {
-        String spacer = "____________________________________________________________";
+        String spacer = "   ____________________________________________________________";
         System.out.println(spacer);
-        System.out.println("Hello! I'm Tomato \nWhat can I do for you?");
+        System.out.println("    Hello! I'm Tomato \uD83C\uDF45 ! \n    What can I do for you?");
         System.out.println(spacer);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        // loop logic until exiting
+        while(sc.hasNextLine()) {
+            input = sc.nextLine();
+            if(input.equals("bye")) break;
+
+            System.out.println(spacer);
+            System.out.println("    " + input);
+            System.out.println(spacer);
+        }
+        System.out.println(spacer);
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(spacer);
     }
 }
